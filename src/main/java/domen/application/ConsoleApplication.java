acdc2865bc25908045977adbde9f9e0057dda097
desser2002 <oxxxymironushe@gmail.com>
2025-05-24 19:06:57 +0200

    Consoleapp class

---
+++ src/main/java/domen/application/ConsoleApplication.java
@@ -0,0 +1,20 @@
+package domen.application;
+
+import domen.domain.SubtaskService;
+import domen.domain.TaskReportService;
+import domen.domain.TaskService;
+
+public class ConsoleApplication {
+    private final TaskService taskService;
+    private final TaskReportService taskReportService;
+    private final SubtaskService subtaskService;
+
+    public ConsoleApplication(TaskService taskService, TaskReportService taskReportService, SubtaskService subtaskService) {
+        this.taskService = taskService;
+        this.taskReportService = taskReportService;
+        this.subtaskService = subtaskService;
+    }
+
+    public void run() {
+    }
+}
